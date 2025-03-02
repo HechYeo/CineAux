3ef0e45d15c450fecc80fdaa5ad7bb59ed6a8e86
Viet Hung Nguyen <viethungtv87@gmail.com>
2025-03-03 00:37:45 +0100

    Anpassungen

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # FitnessApp
 Fitness App für WPF Python
+
+
+zum Starten der venv:
+- cd Fitnessapp/fitnessapp
+- .\venv\Structure\Activate
+
+anschließend Server starten:
+- python manage.py runserver
+
+Anschließend URL öffnen:
+http://127.0.0.1:8000/
--- fitnessapp/fitnessapp/urls.py
+++ fitnessapp/fitnessapp/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 # fitnessapp/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/", include("users.urls")),
+    path('', lambda request: redirect('/users/login/')),
 ]
